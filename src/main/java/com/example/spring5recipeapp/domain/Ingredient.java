285d44c848e226ac225de2c354ec42d43fe8a3dc
PhNorberg <philipnorberg@gmail.com>
2022-08-11 08:34:57 +0200

    142 Assignment - Display List of Recipes

--- src/main/java/com/example/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/com/example/spring5recipeapp/domain/Ingredient.java
@@ -12,13 +12,20 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
-
     @ManyToOne
     private Recipe recipe;
 
     @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure unitOfMeasure;
+
+    public Ingredient() {
+    }
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.unitOfMeasure = unitOfMeasure;
+        this.recipe = recipe;
+    }
 
     public Long getId() {
         return id;
--- src/main/java/com/example/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/example/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.example.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,6 +17,8 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @Lob
@@ -26,7 +29,7 @@ public class Recipe {
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     // mappedBy = "recipe" connects the recipe member variable in the Ingredient for the relationship
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
@@ -35,7 +38,7 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
     joinColumns = @JoinColumn(name = "recipe_id"),
     inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
     public Long getId() {
         return id;
     }
